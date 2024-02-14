6e9eaaa44df886a86409a8c26aff39f9d3437185
Justin Shakergayen <jshakerg@purdue.edu>
2024-02-14 14:34:48 -0500

    changed some settings around

--- signs/models.py
+++ signs/models.py
@@ -1,28 +1,20 @@
 from django.db import models
+from django.conf import settings
 
+class Tag(models.Model):
+    name = models.CharField(max_length=200, default = "")
 
-"""
-A sign is an asl sign that has certain parameters that can be used to sort it.
-We sort by charfields as not to limit ourselves at multiple instances.
-"""
 class Sign(models.Model):
     sign_name = models.CharField(max_length=200)
     pub_date = models.DateTimeField("date published")
-    handshape = models.CharField(max_length=200)
-    location = models.CharField(max_length=200)
-    palm_orientation = models.CharField(max_length=200)
-    movement = models.BooleanField()
+    tags = models.ManyToManyField(Tag)
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null = True)
 
-    #created_by = models.ForeignKey()
-
-"""
-Demonstrations are attached to signs. They can be images or videos.
-"""
 class Demonstration(models.Model):
-    #user
     pub_date = models.DateTimeField("date published")
     sign = models.ForeignKey(Sign, on_delete=models.CASCADE)
-
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null = True)
 
 class Set(models.Model):
-    contained_signs = models.ManyToManyField("Sign", related_name = "signs")
+    contained_signs = models.ManyToManyField("Sign", related_name="signs")
+    created_by = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, null = True)

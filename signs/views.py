86d42ca32799347c5b8b8f0d5534e0c49a5867e1
Justin Shakergayen <jshakerg@purdue.edu>
2024-04-03 12:45:57 -0400

    Users

--- signs/views.py
+++ signs/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
 # Create your views here.
-class UserFilterView(TemplateView):
+class SignFilterView(TemplateView):
     template_name = 'signs/index.html'
--- users/urls.py
+++ users/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 from . import views
 # Create your views here.
 urlpatterns = [
-    path("", views.SignFilterView.as_view(), name = "user-filter" )
+    path("", views.UserFilterView.as_view(), name = "user-filter" )
 ]
